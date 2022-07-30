19a71213ba499425f2be77dd447be58a96f7cf87
omaraziz2006 <109866664+omaraziz2006@users.noreply.github.com>
2022-07-30 14:52:44 +0300

    cw2 is done

---
+++ cw2/Assets/help.cs
@@ -0,0 +1,13 @@
+// ints and stuff
+string heroname="ragnar";
+string villanname="hessa";
+int heroage=9;
+int villanage=10000;
+
+
+// print the code 
+
+ void   Print("once there was a hero named" + heroname);
+        Print("and a villan whose name is" + villanname);
+        Print("the heros age is" + heroage + "and the villan name is" + villanage);
+        Print("the age differece between them is" + villanage - heroage); 
